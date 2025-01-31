1ce15ca3c0a9449ac6dae730eb25db0fa82a1d6f
mrhero05 <ogiesanchez.hs@gmail.com>
2025-01-31 09:47:49 +0800

    install vue js package

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="flex justify-center my-5">
+        <img src="@/assets/images/Logo.png" alt="Github Explorer Logo">
+    </section>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,12 @@
+<template>
+    <section>
+        <div class="border-0 -text--ge-white-gray">
+            <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+            <p>Search User or Repository</p>
+        </div>
+    </section>
+</template>
+
+<script setup>
+
+</script>
