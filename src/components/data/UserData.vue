9e8f6821efef3cfd9db030081628dd96776c73ca
mrhero05 <ogiesanchez.hs@gmail.com>
2025-02-15 22:31:12 +0800

    close the modal when user select result

--- src/components/data/UserData.vue
+++ src/components/data/UserData.vue
@@ -6,7 +6,7 @@
         avatar: user.avatar_url,
         github_url: user.html_url,
     }
-    }" class="flex items-center p-[10px_15px] rounded-[10px] gap-x-[10px] my-4 hover:outline hover:-outline--ge-sky-blue hover:cursor-pointer hover:-text--ge-sky-blue transition-[all_150ms_ease]" v-if="userData" v-for="user in userData.slice(0, itemToShow)" @click="selectUser">
+    }" class="flex items-center p-[10px_15px] rounded-[10px] gap-x-[10px] my-4 hover:outline hover:-outline--ge-sky-blue hover:cursor-pointer hover:-text--ge-sky-blue transition-[all_150ms_ease]" v-if="userData" v-for="user in userData.slice(0, itemToShow)" @click="closeModal">
         <font-awesome-icon class="" :icon="['fas', 'hashtag']" />
         <img :src="user.avatar_url" class="w-[50px] h-auto" alt="github_avatar">
         <p class="">{{ user.login }}</p>
@@ -22,7 +22,9 @@
         itemToShow: Number
     });
 
-    const selectUser = () => {
+    const closeModal = () => {
+        const modal = document.getElementById('searchModal');
+        modal.close();
     }
 
 </script>
