2c1a9f5edd995a37aec39a059b075fe5edc36450
mrhero05 <ogiesanchez.hs@gmail.com>
2025-03-11 11:41:35 +0800

    Add langunage percentage

--- src/components/content/repo/RepoLanguages.vue
+++ src/components/content/repo/RepoLanguages.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { defineProps, ref, watch, onMounted } from 'vue';
+    import { defineProps, ref, watch, onMounted, onUnmounted } from 'vue';
 
     const props = defineProps({
         repoName: String,
@@ -8,48 +8,57 @@
     });
 
     let repoLanguageFinalData = ref([]);
-    let repoTotalPercentage = ref();
 
-    const getLanguageData = (repoLanguagesColor, repoLanguagesData) =>{
+    const getLanguageData = (repoLanguagesColor, repoLanguagesData, repoName) =>{
+        let langTotal = 0;
+        for (let langName in repoLanguagesData) {
+            langTotal += repoLanguagesData[langName];
+        }
+
         repoLanguageFinalData.value = repoLanguagesColor.map(lang =>{
             const langName = Object.keys(lang)[0];
             const langColor = Object.values(lang)[0];
-            const langPercentage = getLanguagePercentage(repoLanguagesData[langName]);
+            const langPercentage = getLanguagePercentage(repoLanguagesData[langName], langTotal);
+
             return {
                 language : langName,
                 percentage : langPercentage,
                 color : langColor,
             }
         });
-        console.log(repoLanguageFinalData);
 
+        console.log(repoLanguageFinalData.value);
     }
 
-    const getLanguagePercentage = (value) =>{
-        return value;
+    const getLanguagePercentage = (value, total) =>{
+        return (value / total) * 100;
     }
 
-    watch(() => props.repoName, (newRepoName) =>{
-        if(newRepoName){
-            getLanguageData(props.repoLanguagesColor, props.repoLanguagesData);
+    watch(() => props.repoLanguagesColor,
+     (newRepoLangColor) =>{
+        if(newRepoLangColor){
+            getLanguageData(newRepoLangColor, props.repoLanguagesData, props.repoName);
         }
     });
 
     onMounted(() => {
-        getLanguageData(props.repoLanguagesColor, props.repoLanguagesData);
+        getLanguageData(props.repoLanguagesColor, props.repoLanguagesData, props.repoName);
     });
 
 </script>
 
 <template>
     <div class="flex h-4 w-full -bg--ge-light-gray rounded-full overflow-hidden">
-        <div class="h-full w-[20%]"
+        <div class="h-full"
         v-if="repoLanguageFinalData"
-        :style="{backgroundColor: repoData.color}"
-        v-for="repoData in repoLanguageFinalData" :key="repoData"></div>
+        :style="{
+            backgroundColor: repoData.color,
+            width: `${repoData.percentage}%`
+        }"
+        v-for="repoData in repoLanguageFinalData" :key="repoData.language"></div>
     </div>
     <div class="flex">
-        <div v-for="repoData in repoLanguageFinalData" :key="repoData">
+        <div v-for="repoData in repoLanguageFinalData" :key="repoData.language">
             <p>{{ repoData.language }}</p>
             <p>{{ repoData.percentage }}</p>
         </div>
